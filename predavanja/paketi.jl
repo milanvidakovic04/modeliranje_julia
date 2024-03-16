769f703db320170dd53afa517ab7329ebb583cc2
Milan <125221110+milanvidakovic04@users.noreply.github.com>
2024-03-16 15:05:53 +0100

    Delete predavanja directory

--- predavanja/paketi.jl
+++
@@ -1,6 +0,0 @@
-#primjer instaliranja i koristenja paketa
-using Pkg  #uvijek treba prvo pokrenuti modul Packages . Pkg
-Pkg.status() #naredba omogućuje nam da provjerimo koje pakete imamo instalirane
-Pkg.update() #naredba radi update paketa
-Pkg.add("Plots") #ovom nardebom dodajemo nove sluzbene pakete
-using Plots #kada paket imamo dodan, prije svakog korištenja moramo koristiti ovu naredbu
